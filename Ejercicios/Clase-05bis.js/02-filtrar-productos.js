3abbe90f1fcea8abcd3e72cdac906988339fd731
Santino <101665218+Santinou1@users.noreply.github.com>
2023-06-15 20:32:45 -0300

    Delete 02-filtrar-productos.js

--- Ejercicios/Clase-05bis.js/02-filtrar-productos.js
+++
@@ -1,4 +0,0 @@
-//02- Filtrar productos por categoría
-//Este ejercicio tiene que filtrar y muestra solo los productos de una categoría específica, pasada como argumento por process.argv.
-//{node 02 categoria}
-
